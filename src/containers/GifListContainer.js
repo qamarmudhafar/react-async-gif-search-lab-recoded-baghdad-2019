56e78b1bf8e6af661a425c877293cf7e8a10adf7
qamar mudhafar <qamarmudhafar@yahoo.com>
2020-01-16 15:41:48 +0000

    Automatically backed up by Learn

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -1,7 +1,10 @@
-import React from 'react' 
-import GifList from '../components/GifList.js'
-import GifSearch from '../components/GifSearch'
+import React from 'react' ;
+import GifList from '../components/GifList.js';
+import GifSearch from '../components/GifSearch';
 
 export default class GifListContainer extends React.component {
   
+  state {
+    gif = []
+  }
 }
